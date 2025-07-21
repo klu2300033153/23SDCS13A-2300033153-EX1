0932b7d289128a520571f978d5b78a8cf2cd382a
klu2300033153 <2300033153@kluniversity.in>
2025-07-21 10:12:07 +0530

    Update: Root Folder

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename='/23SDCS13A-2300033153-EX1/frontend'>
+    <Router basename='/23SDCS13A-2300033153-EX1'>
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
