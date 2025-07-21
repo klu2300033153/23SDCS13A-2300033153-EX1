19fcef95977a3519c7244371f7c241f4642f349d
klu2300033153 <2300033153@kluniversity.in>
2025-07-21 23:01:24 +0530

    Fix: All Issues

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/23SDCS13A-2300033153-EX1'
-})
+  base: '/23SDCS13A-2300033153-EX1/'
+});
